6b347e26c5c901f5be59b60cb74b96bfa659a4db
Scott <scottboragay06@gmail.com>
2025-09-25 18:24:23 +0800

    variables and echo printing

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php testing embedded</title>
+</head>
+<body>
+
+    
+</body>
+
+    <?php
+        echo "hello world";
+
+        $greet = "hello world" ;
+        echo $greet;
+    ?>
+
+</html>
