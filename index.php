921598b91cdad96fd1c3f8a084237973aa3b5b49
Scott <scottboragay06@gmail.com>
2025-09-25 19:02:14 +0800

    added: heredoc and concat with .

--- index.php
+++ index.php
@@ -1,26 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php testing embedded</title>
 </head>
+
 <body>
 
-    
+
 </body>
 
-    <?php
+<?php
 
 
 
-        const greetings = "hello world";
-        echo greetings;
+$greetings = <<<start
+asdasdasdasd
+asdasdasd
+asdasd
+start;
 
-        $x = 0;
-        $calcu = $x++ + 1;
-        echo $calcu;
 
-    ?>
+echo $greetings;
+
+$x = 0;
+$calcu = $x++ + 1;
+echo $calcu;
+
+
+
+$str1 = "hello";
+$str2 = "world";
+echo $str1 . $str2;
+?>
 
 </html>
